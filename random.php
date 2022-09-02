58104720b538d8437fff2dff7124f0753eec2cd9
As <64366086+AstralSnake@users.noreply.github.com>
2022-09-02 20:56:05 +0300

    Add files via upload

---
+++ random.php
@@ -0,0 +1,13 @@
+<?php
+$var = rand(1,3);
+
+if($var == 1){
+	print '<img src="https://sodas.neocities.org/random_img/sodas1.jpg">';
+}
+if($var == 2){
+	print '<img src="https://sodas.neocities.org/random_img/sodas2.jpg">';
+}
+if($var == 3){
+	print '<img src="https://sodas.neocities.org/random_img/sodas3.jpg">';
+}
+?>
